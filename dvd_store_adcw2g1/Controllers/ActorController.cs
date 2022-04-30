148139509b22b6470d64ad9ccef0b8e7590444ee
Sujan901293 <suzanpud@gmail.com>
2022-04-30 21:05:39 +0545

    Actor CRUD

---
+++ dvd_store_adcw2g1/Controllers/ActorController.cs
@@ -0,0 +1,92 @@
+﻿using dvd_store_adcw2g1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace dvd_store_adcw2g1.Controllers
+{
+    public class ActorController : Controller
+    {
+
+        private readonly DatabaseContext _databasecontext;
+
+        public ActorController(DatabaseContext context)
+        {
+            _databasecontext = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _databasecontext.Actors.ToListAsync());
+        }
+
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+
+        }
+
+       [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Actor actor)
+  
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _databasecontext.Add(actor);
+                    await _databasecontext.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
+            }
+            return View(actor);
+        }
+
+        public async Task<IActionResult> EditPost(int id)
+        {
+            var studentToUpdate = await _databasecontext.Actors.SingleOrDefaultAsync(s => s.ActorNumber == id);
+            return View(studentToUpdate);
+        }
+
+
+            [HttpPost, ActionName("EditPost")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditPost(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var studentToUpdate = await _databasecontext.Actors.FirstOrDefaultAsync(s => s.ActorNumber == id);
+            if (await TryUpdateModelAsync<Actor>(
+                studentToUpdate,
+                "",
+                s => s.ActorSurname, s => s.ActorFirstName))
+            {
+                try
+                {
+                    await _databasecontext.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException /* ex */)
+                {
+                    //Log the error (uncomment ex variable name and write a log.)
+                    ModelState.AddModelError("", "Unable to save changes. " +
+                        "Try again, and if the problem persists, " +
+                        "see your system administrator.");
+                }
+            }
+            return View(studentToUpdate);
+        }
+
+
+
+    }
+}
--- dvd_store_adcw2g1/Models/Actor.cs
+++ dvd_store_adcw2g1/Models/Actor.cs
@@ -1,10 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace dvd_store_adcw2g1.Models
 {
     public class Actor
     {
         [Key]
+
+        [DatabaseGenerated(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)]
         public int ActorNumber { get; set; }
 
         public string ActorSurname { get; set; }
