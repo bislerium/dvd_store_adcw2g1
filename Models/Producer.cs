3fe657dc4e1b032494278193637371c21bfa0a8e
Bishal Gharti Chhetri <chhetrig.bishal@gmail.com>
2022-05-02 12:20:27 +0545

    Restructured

---
+++ Models/Producer.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dvd_store_adcw2g1.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int ProducerNumber { get; set; }
+
+        public string ProducerName { get; set; }
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dvd_store_adcw2g1.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)]
+        public int UserNumber { get; set; }
+
+        public string UserName { get; set; }
+
+        public string UserType { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 5,
+           ErrorMessage = "Title must be between 5 and 100 characters long")]
+        public string UserPassword { get; set; }
+
+    }
+}
