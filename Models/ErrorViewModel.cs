4e0164c670817365e3051c4a07466b8227c46e76
Bishal Gharti Chhetri <chhetrig.bishal@gmail.com>
2022-05-09 21:12:45 +0545

    second attempt

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace dvd_store_adcw2g1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
